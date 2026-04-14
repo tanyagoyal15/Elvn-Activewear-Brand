8958e97e56e2302526712e85be2aee0a52734e8e
Tanya Goyal <tanyagoyal15@gmail.com>
2026-04-14 11:21:54 +0530

    chore: add shopify cli config and push-dev script

---
+++ push-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Pushing to DEV theme only..."
+shopify theme push --store=ejjge0-zf.myshopify.com --theme=160971161842
+echo "Done. Preview: https://ejjge0-zf.myshopify.com/?preview_theme_id=160971161842"
